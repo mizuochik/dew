da463a03454f6d9c92cdf789516e79d72b96a153
Mizuochi Keita <keitam913@yahoo.co.jp>
2023-06-24 16:30:41 +0900

    Enable raw mode

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,9 @@ const os = std.os;
 const io = std.io;
 
 pub fn main() !void {
+    const orig = try enableRawMode();
+    defer disableRawMode(orig) catch unreachable;
+
     var buf = [_]u8{0} ** 32;
     while (io.getStdIn().read(&buf)) |_| {
         if (buf[0] == 'q')
@@ -10,4 +13,16 @@ pub fn main() !void {
     } else |err| {
         return err;
     }
+}
+
+fn enableRawMode() !os.termios {
+    const orig = try os.tcgetattr(os.STDIN_FILENO);
+    var term = orig;
+    term.lflag &= ~os.linux.ECHO;
+    try os.tcsetattr(os.STDIN_FILENO, os.TCSA.FLUSH, term);
+    return orig;
+}
+
+fn disableRawMode(orig: os.termios) !void {
+    try os.tcsetattr(os.STDIN_FILENO, os.TCSA.FLUSH, orig);
 }
