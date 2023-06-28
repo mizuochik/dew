f9b4d50ff1773b8dc5c987f91cc75944ee963264
Mizuochi Keita <keitam913@yahoo.co.jp>
2023-06-28 21:29:35 +0900

    Support opening arbitrary files

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,11 @@
 const std = @import("std");
 const heap = std.heap;
+const os = std.os;
+const fmt = std.fmt;
+const io = std.io;
+const time = std.time;
 const debug = std.debug;
+const mem = std.mem;
 const Editor = @import("Editor.zig");
 
 pub fn main() !void {
@@ -8,11 +13,16 @@ pub fn main() !void {
     defer debug.assert(gpa.deinit() == .ok);
     const allocator = gpa.allocator();
 
+    if (os.argv.len < 2) {
+        try io.getStdErr().writer().print("Specify file\n", .{});
+        os.exit(1);
+    }
+    const path: []const u8 = mem.span(os.argv[1]);
+
     var editor = try Editor.init(allocator);
     defer editor.deinit() catch unreachable;
 
-    try editor.openFile("build.zig");
-
+    try editor.openFile(path);
     try editor.run();
 }
 
