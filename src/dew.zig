527b0421e8a74e7e85e58f37cb586fa08572b2d9
Mizuochi Keita <keitam913@yahoo.co.jp>
2023-07-09 13:20:42 +0900

    UnicodeLineBuffer: Add insert

--- src/dew.zig
+++ src/dew.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 pub const Editor = @import("dew/Editor.zig");
+pub const UnicodeLineBuffer = @import("dew/UnicodeLineBuffer.zig");
 pub const c = @import("dew/c.zig");
 
 test {
---
+++ src/dew/UnicodeLineBuffer.zig
@@ -0,0 +1,57 @@
+const std = @import("std");
+const mem = std.mem;
+const unicode = std.unicode;
+const testing = std.testing;
+const UnicodeLineBuffer = @This();
+
+buffer: std.ArrayList(u8),
+u8_index: std.ArrayList(usize),
+
+pub fn init(allocator: mem.Allocator) !UnicodeLineBuffer {
+    var buf = UnicodeLineBuffer{
+        .buffer = std.ArrayList(u8).init(allocator),
+        .u8_index = std.ArrayList(usize).init(allocator),
+    };
+    try buf.refreshCodePoints();
+    return buf;
+}
+
+pub fn deinit(self: *UnicodeLineBuffer) void {
+    self.buffer.deinit();
+    self.u8_index.deinit();
+}
+
+pub fn insert(self: *UnicodeLineBuffer, i: usize, c: u21) !void {
+    var enc: [4]u8 = undefined;
+    const to = try unicode.utf8Encode(c, &enc);
+    try self.buffer.insertSlice(self.u8_index.items[i], enc[0..to]);
+    try self.refreshCodePoints();
+}
+
+pub fn refreshCodePoints(self: *UnicodeLineBuffer) !void {
+    var new_u8_index = std.ArrayList(usize).init(self.buffer.allocator);
+    errdefer new_u8_index.deinit();
+
+    const view = try unicode.Utf8View.init(self.buffer.items);
+    var it = view.iterator();
+
+    try new_u8_index.append(it.i);
+    while (it.nextCodepoint()) |_| {
+        try new_u8_index.append(it.i);
+    }
+
+    self.u8_index.deinit();
+    self.u8_index = new_u8_index;
+}
+
+test "UnicodeLineBuffer: insert" {
+    var lb = try UnicodeLineBuffer.init(testing.allocator);
+    defer lb.deinit();
+
+    try lb.insert(0, '世');
+    try lb.insert(1, '界');
+
+    try testing.expectFmt("世界", "{s}", .{lb.buffer.items});
+    const cps: []const usize = &[_]usize{ 0, 3, 6 };
+    try testing.expectEqualDeep(cps, lb.u8_index.items);
+}
