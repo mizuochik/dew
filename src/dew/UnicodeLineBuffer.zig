4d708b592a94b806fee7ae3383352773990812b8
Mizuochi Keita <keitam913@yahoo.co.jp>
2023-07-11 20:51:39 +0900

    UnicodeLineBuffer: Add index of cursor pos

--- src/dew/UnicodeLineBuffer.zig
+++ src/dew/UnicodeLineBuffer.zig
@@ -6,31 +6,34 @@ const UnicodeLineBuffer = @This();
 
 buffer: std.ArrayList(u8),
 u8_index: std.ArrayList(usize),
+cursor_index: std.ArrayList(usize),
 
 pub fn init(allocator: mem.Allocator) !UnicodeLineBuffer {
     var buf = UnicodeLineBuffer{
         .buffer = std.ArrayList(u8).init(allocator),
         .u8_index = std.ArrayList(usize).init(allocator),
+        .cursor_index = std.ArrayList(usize).init(allocator),
     };
-    try buf.refreshU8Index();
+    try buf.refreshIndex();
     return buf;
 }
 
 pub fn deinit(self: *const UnicodeLineBuffer) void {
     self.buffer.deinit();
     self.u8_index.deinit();
+    self.cursor_index.deinit();
 }
 
 pub fn insert(self: *UnicodeLineBuffer, i: usize, c: u21) !void {
     var enc: [4]u8 = undefined;
     const to = try unicode.utf8Encode(c, &enc);
     try self.buffer.insertSlice(self.u8_index.items[i], enc[0..to]);
-    try self.refreshU8Index();
+    try self.refreshIndex();
 }
 
 pub fn appendSlice(self: *UnicodeLineBuffer, s: []const u8) !void {
     try self.buffer.appendSlice(s);
-    try self.refreshU8Index();
+    try self.refreshIndex();
 }
 
 pub fn remove(self: *UnicodeLineBuffer, i: usize) !void {
@@ -39,10 +42,15 @@ pub fn remove(self: *UnicodeLineBuffer, i: usize) !void {
     for (from..to) |_| {
         _ = self.buffer.orderedRemove(from);
     }
-    try self.refreshU8Index();
+    try self.refreshIndex();
 }
 
-pub fn refreshU8Index(self: *UnicodeLineBuffer) !void {
+fn refreshIndex(self: *UnicodeLineBuffer) !void {
+    try self.refreshU8Index();
+    try self.refreshCursorIndex();
+}
+
+fn refreshU8Index(self: *UnicodeLineBuffer) !void {
     var new_u8_index = std.ArrayList(usize).init(self.buffer.allocator);
     errdefer new_u8_index.deinit();
 
@@ -58,6 +66,19 @@ pub fn refreshU8Index(self: *UnicodeLineBuffer) !void {
     self.u8_index = new_u8_index;
 }
 
+fn refreshCursorIndex(self: *UnicodeLineBuffer) !void {
+    var new_cursor_index = std.ArrayList(usize).init(self.buffer.allocator);
+    errdefer new_cursor_index.deinit();
+    var j: usize = 0;
+    try new_cursor_index.append(j);
+    for (0..self.u8_index.items.len - 1) |i| {
+        j += if (self.u8_index.items[i + 1] - self.u8_index.items[i] > 1) 2 else 1;
+        try new_cursor_index.append(j);
+    }
+    self.cursor_index.deinit();
+    self.cursor_index = new_cursor_index;
+}
+
 test "UnicodeLineBuffer: insert" {
     var lb = try UnicodeLineBuffer.init(testing.allocator);
     defer lb.deinit();
@@ -67,6 +88,7 @@ test "UnicodeLineBuffer: insert" {
 
     try testing.expectFmt("世界", "{s}", .{lb.buffer.items});
     try testing.expectFmt("{ 0, 3, 6 }", "{any}", .{lb.u8_index.items});
+    try testing.expectFmt("{ 0, 2, 4 }", "{any}", .{lb.cursor_index.items});
 }
 
 test "UnicodeLineBuffer: remove" {
@@ -79,4 +101,5 @@ test "UnicodeLineBuffer: remove" {
 
     try testing.expectFmt("こんちは", "{s}", .{lb.buffer.items});
     try testing.expectFmt("{ 0, 3, 6, 9, 12 }", "{any}", .{lb.u8_index.items});
+    try testing.expectFmt("{ 0, 2, 4, 6, 8 }", "{any}", .{lb.cursor_index.items});
 }
