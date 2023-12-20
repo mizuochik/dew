101c47e0798e98ad33ba7c82224ad44aa211369a
Mizuochi Keita <keitam913@yahoo.co.jp>
2023-12-20 16:09:00 +0900

    e2e/cursor: Add a case of moving cursor vertically

--- src/e2e/cursor.zig
+++ src/e2e/cursor.zig
@@ -41,3 +41,37 @@ test "move to the beginning or end of line" {
     try editor.controller.processKeypress(.{ .ctrl = 'E' });
     try std.testing.expectEqualDeep(models.Position{ .x = 100, .y = 0 }, cursor.getPosition());
 }
+
+test "move vertically considering double bytes" {
+    {
+        const editor = try Editor.init(std.testing.allocator);
+        defer editor.deinit();
+        try editor.controller.changeDisplaySize(100, 100);
+        const cursor = &editor.controller.buffer_selector.file_buffer.cursors.items[0];
+        try editor.buffer_selector.file_buffer.openFile("src/e2e/mixed-byte-lines.txt");
+
+        // Move to first half of double byte character
+        for (0..4) |_| try editor.controller.processKeypress(.{ .arrow = .right });
+        try std.testing.expectEqual(models.Position{ .x = 4, .y = 0 }, cursor.getPosition());
+        try editor.controller.processKeypress(.{ .arrow = .down });
+        try std.testing.expectEqual(models.Position{ .x = 2, .y = 1 }, cursor.getPosition());
+        try editor.controller.processKeypress(.{ .arrow = .up });
+        try std.testing.expectEqual(models.Position{ .x = 4, .y = 0 }, cursor.getPosition());
+    }
+    {
+        const editor = try Editor.init(std.testing.allocator);
+        defer editor.deinit();
+        try editor.controller.changeDisplaySize(100, 100);
+        const cursor = &editor.controller.buffer_selector.file_buffer.cursors.items[0];
+        try editor.buffer_selector.file_buffer.openFile("src/e2e/mixed-byte-lines.txt");
+
+        // Move to back half of double byte character
+        for (0..5) |_| try editor.controller.processKeypress(.{ .arrow = .right });
+        try std.testing.expectEqual(models.Position{ .x = 5, .y = 0 }, cursor.getPosition());
+        try editor.controller.processKeypress(.{ .arrow = .down });
+        try std.testing.expectFmt("(2, 1)", "{}", .{cursor.getPosition()});
+        try std.testing.expectEqual(models.Position{ .x = 2, .y = 1 }, cursor.getPosition());
+        try editor.controller.processKeypress(.{ .arrow = .up });
+        try std.testing.expectEqual(models.Position{ .x = 5, .y = 0 }, cursor.getPosition());
+    }
+}
