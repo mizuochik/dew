d40a200ce182aa05d422625e8709893f901f74c6
Mizuochi Keita <keitam913@yahoo.co.jp>
2023-12-23 23:12:17 +0900

    build.zig: Add clap

--- build.zig
+++ build.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
@@ -11,17 +10,18 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+    exe.addModule("clap", b.dependency("clap", .{
+        .target = target,
+        .optimize = optimize,
+    }).module("clap"));
 
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
-
     run_cmd.step.dependOn(b.getInstallStep());
-
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
@@ -30,9 +30,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-
     const run_tests = b.addRunArtifact(tests);
-
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_tests.step);
 }
