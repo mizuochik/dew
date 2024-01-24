9875d1a080412b34f6d32d04053a846d2fa76f8f
Mizuochi Keita <keitam913@yahoo.co.jp>
2024-01-25 04:55:50 +0900

    builtin_resources.cursors: New

--- src/builtin_resources.zig
+++ src/builtin_resources.zig
@@ -1,2 +1,3 @@
 pub const editor = @import("builtin_resources/editor.zig");
 pub const files = @import("builtin_resources/files.zig");
+pub const cursors = @import("builtin_resources/cursors.zig");
---
+++ src/builtin_resources/cursors.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+const Editor = @import("../Editor.zig");
+const Resource = @import("../Resource.zig");
+const EditView = @import("../EditView.zig");
+
+pub fn init(allocator: std.mem.Allocator) !Resource {
+    var cursors = Resource.init(allocator);
+    errdefer cursors.deinit();
+    return cursors;
+}
+
+fn moveTo(editor: *Editor, arguments: [][]const u8) anyerror!void {
+    if (arguments.len != 1) {
+        const message = try std.fmt.allocPrint(editor.allocator, "invalid argument length: want {} but {}", .{ 1, arguments.len });
+        errdefer editor.allocator.free(message);
+        try editor.client.status.setMessage(message);
+        return error.InvalidArguments;
+    }
+    const location = arguments[0];
+    var edit = editor.client.getActiveEdit() orelse return;
+    if (std.mem.eql(u8, location, "next-character")) {
+        try edit.cursor.moveForward();
+        return;
+    }
+    if (std.mem.eql(u8, location, "previous-character")) {
+        try edit.cursor.moveBackward();
+        return;
+    }
+    const view = getCurrentView(editor);
+    const view_y = view.getCursor(edit).y;
+    if (std.mem.eql(u8, location, "next-line")) {
+        if (view_y >= view.getNumberOfLines() - 1) {
+            return;
+        }
+        const pos = view.getBufferPosition(edit, .{ .x = edit.cursor.last_view_x, .y = view_y + 1 });
+        try edit.cursor.setPosition(pos);
+    }
+    if (std.mem.eql(u8, location, "previous-line")) {
+        if (view_y <= 0) {
+            return;
+        }
+        const pos = view.getBufferPosition(edit, .{ .x = edit.cursor.last_view_x, .y = view_y - 1 });
+        try edit.cursor.setPosition(pos);
+    }
+}
+
+fn getCurrentView(editor: *Editor) *EditView {
+    return if (editor.client.is_command_line_active)
+        editor.command_edit_view
+    else
+        editor.edit_view;
+}
