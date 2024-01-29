ca3eee17806cbe5a596e13bb1272f1def396e38d
Mizuochi Keita <keitam913@yahoo.co.jp>
2024-01-30 04:32:04 +0900

    Edit: New for editing operations

---
+++ src/Edit.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+const TextRef = @import("TextRef.zig");
+const Client = @import("Client.zig");
+
+active_ref: *TextRef,
+client: *const Client,
+
+pub fn isCommandLineActive(self: *const @This()) bool {
+    return self.active_ref == &self.client.command_line_edit;
+}
--- src/tests.zig
+++ src/tests.zig
@@ -7,6 +7,7 @@ test {
         pub const Terminal = @import("Terminal.zig");
         pub const Client = @import("Client.zig");
         pub const TextRef = @import("TextRef.zig");
+        pub const Edit = @import("Edit.zig");
         pub const Resource = @import("Resource.zig");
         pub const CommandParser = @import("CommandParser.zig");
         pub const ResourceRegistry = @import("ResourceRegistry.zig");
