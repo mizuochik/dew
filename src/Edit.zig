ab16bafc4e68a41a776d6e19cd56c25b1ae7266e
Mizuochi Keita <keitam913@yahoo.co.jp>
2024-01-30 05:29:51 +0900

    Edit: Simplify fields

--- src/Edit.zig
+++ src/Edit.zig
@@ -3,9 +3,4 @@ const std = @import("std");
 const TextRef = @import("TextRef.zig");
 const Client = @import("Client.zig");
 
-active_ref: *TextRef,
-client: *const Client,
-
-pub fn isCommandLineActive(self: *const @This()) bool {
-    return self.active_ref == &self.client.command_line_ref;
-}
+active_ref: ?*TextRef = null,
