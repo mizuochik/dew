c8b0fd29c6dab2dc9e4e15b99179fa0dbed9f825
Mizuochi Keita <keitam913@yahoo.co.jp>
2024-02-01 06:01:18 +0900

    command_line: Support the put method

--- src/builtin_resources/command_line.zig
+++ src/builtin_resources/command_line.zig
@@ -7,9 +7,24 @@ pub fn init(allocator: std.mem.Allocator) !Resource {
     var command_line = Resource.init(allocator);
     errdefer command_line.deinit();
     try command_line.putMethod("toggle", toggle);
+    try command_line.putMethod("put", put);
     return command_line;
 }
 
 fn toggle(editor: *Editor, _: [][]const u8) !void {
     try editor.client.toggleCommandLine();
+}
+
+fn put(editor: *Editor, params: [][]const u8) !void {
+    const want_params_len = 1;
+    if (params.len != want_params_len) {
+        const message = try std.fmt.allocPrint(editor.allocator, "invalid argument length: want {d} but {d}", .{ want_params_len, params.len });
+        errdefer editor.allocator.free(message);
+        try editor.client.status.setMessage(message);
+        return;
+    }
+    if (!editor.client.isCommandLineActive())
+        try editor.client.toggleCommandLine();
+    try editor.client.command_line.clear();
+    try editor.client.command_line.rows.items[0].appendSlice(params[0]);
 }
