45ae020dadcf4edfe39325edda36aab4c3666622
Mizuochi Keita <keitam913@yahoo.co.jp>
2024-02-01 06:02:12 +0900

    KeyEvaluator: Map C+o to files.open

--- src/KeyEvaluator.zig
+++ src/KeyEvaluator.zig
@@ -76,6 +76,9 @@ pub fn installDefaultKeyMap(self: *@This()) !void {
     try self.putBuiltinKeyMap("del", .{"text.delete-backward-character"});
     try self.putBuiltinKeyMap("C+s", .{"files.save"});
     try self.putBuiltinKeyMap("C+x", .{"command-line.toggle"});
+    try self.putBuiltinKeyMap("C+o", .{
+        \\command-line.put "files.open "
+    });
 }
 
 pub fn putBuiltinKeyMap(self: *@This(), key_name: []const u8, commands: anytype) !void {
