927b3c169013d40104e5921a41adb36aae4a431a
Mizuochi Keita <keitam913@yahoo.co.jp>
2024-02-21 07:48:18 +0900

    Module: Add some parameters and types for command executions

--- src/Module.zig
+++ src/Module.zig
@@ -2,9 +2,23 @@ const std = @import("std");
 const Keyboard = @import("Keyboard.zig");
 const CommandLine = @import("CommandLine.zig");
 
+pub const Error = error{
+    InvalidCommand,
+};
+
 ptr: *anyopaque,
 name: []const u8,
 api: struct {
-    command: *const fn (ptr: *anyopaque, arguments: [][]const u8) anyerror!void,
+    command: *const fn (ptr: *anyopaque, arguments: [][]const u8, input: std.io.AnyReader, output: std.io.AnyWriter) anyerror!void,
 },
-keys: std.AutoHashMap(Keyboard.Key, CommandLine),
+keys: std.AutoHashMap(Keyboard.Key, []const u8),
+vtable: *const VTable,
+
+pub const VTable = struct {
+    deinit: *const fn (ptr: *anyopaque) void,
+};
+
+pub fn deinit(self: *@This()) void {
+    self.vtable.deinit(self.ptr);
+    self.keys.deinit();
+}
