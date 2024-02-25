af4cd69a136442c22887476c386ca4eecd3400f3
Mizuochi Keita <keitam913@yahoo.co.jp>
2024-02-26 07:26:35 +0900

    CommandLineParser: Rewrite with new parser

--- src/CommandParser.zig
+++ src/CommandParser.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const BufferSelector = @import("BufferSelector.zig");
 const Status = @import("Status.zig");
 const CommandLine = @import("CommandLine.zig");
+const parser = @import("parser.zig");
 
 allocator: std.mem.Allocator,
 input: [][]const u8,
@@ -34,123 +35,67 @@ pub fn deinit(self: *const @This()) void {
 }
 
 pub fn parse(self: *@This(), input: []const u8) !CommandLine {
-    try self.setInput(input);
-    const method_name = try self.parseMethodName();
-    errdefer self.allocator.free(method_name);
-    var arg_list = std.ArrayList([]const u8).init(self.allocator);
-    errdefer {
-        for (arg_list.items) |arg| self.allocator.free(arg);
-        arg_list.deinit();
-    }
-    while (true) {
-        self.parseSpaces() catch break;
-        const arg = self.parseArgument() catch break;
-        errdefer self.allocator.free(arg);
-        try arg_list.append(arg);
-    }
-    const args = try arg_list.toOwnedSlice();
-    errdefer self.allocator.free(args);
-    return .{
+    var state: parser.State = .{
+        .input = input,
         .allocator = self.allocator,
-        .method_name = method_name,
-        .params = args,
     };
+    return Parser.commandLine(&state);
 }
 
-fn setInput(self: *@This(), input: []const u8) !void {
-    const view = try std.unicode.Utf8View.init(input);
-    var it = view.iterator();
-    var cps = std.ArrayList([]const u8).init(self.allocator);
-    errdefer {
-        for (cps.items) |cp| {
-            self.allocator.free(cp);
+const Parser = struct {
+    fn commandLine(state: *parser.State) !CommandLine {
+        const in = state.input;
+        errdefer state.input = in;
+        parser.spaces(state) catch {};
+        const method_name = try name(state);
+        errdefer state.allocator.free(method_name);
+        var params = std.ArrayList([]const u8).init(state.allocator);
+        errdefer {
+            for (params.items) |param| state.allocator.free(param);
+            params.deinit();
         }
-        cps.deinit();
-    }
-    while (it.nextCodepointSlice()) |s| {
-        const ss = try std.fmt.allocPrint(self.allocator, "{s}", .{s});
-        errdefer self.allocator.free(ss);
-        try cps.append(ss);
-    }
-    const in = try cps.toOwnedSlice();
-    errdefer self.allocator.free(in);
-    for (self.input) |cp| {
-        self.allocator.free(cp);
-    }
-    self.allocator.free(self.input);
-    self.input = in;
-}
-
-fn parseMethodName(self: *@This()) ![]const u8 {
-    var method_name_al = std.ArrayList(u8).init(self.allocator);
-    defer method_name_al.deinit();
-    try method_name_al.appendSlice(try self.parseAnyLetter());
-    while (self.parseAnyLetter()) |letter| {
-        try method_name_al.appendSlice(letter);
-    } else |_| {}
-    return try method_name_al.toOwnedSlice();
-}
-
-fn parseCharacter(self: *@This(), character: []const u8) !void {
-    if (self.index >= self.input.len) {
-        return ParseError.EOL;
-    }
-    if (!std.mem.eql(u8, self.input[self.index], character)) {
-        return ParseError.Unexpected;
-    }
-    self.index += 1;
-}
-
-fn parseAnyLetter(self: *@This()) ![]const u8 {
-    if (self.index >= self.input.len) {
-        return ParseError.EOL;
-    }
-    if (std.mem.eql(u8, " ", self.input[self.index])) {
-        return ParseError.Unexpected;
-    }
-    const r = self.input[self.index];
-    self.index += 1;
-    return r;
-}
-
-fn parseAnyCharacter(self: *@This()) ![]const u8 {
-    if (self.index >= self.input.len) {
-        return ParseError.EOL;
-    }
-    const r = self.input[self.index];
-    self.index += 1;
-    return r;
-}
-
-fn parseSpaces(self: *@This()) !void {
-    if (self.index >= self.input.len) {
-        return ParseError.EOL;
-    }
-    if (!std.mem.eql(u8, " ", self.input[self.index])) {
-        return ParseError.Unexpected;
-    }
-    self.index += 1;
-    while (self.index < self.input.len and std.mem.eql(u8, " ", self.input[self.index])) {
-        self.index += 1;
-    }
-}
-
-fn parseArgument(self: *@This()) ![]const u8 {
-    var cmd = std.ArrayList(u8).init(self.allocator);
-    errdefer cmd.deinit();
-    if (self.parseCharacter("\"")) {
         while (true) {
-            if (self.parseCharacter("\"")) {
-                break;
-            } else |_| {}
-            const l = self.parseAnyCharacter() catch break;
-            try cmd.appendSlice(l);
+            parser.spaces(state) catch break;
+            const arg = argument(state) catch doubleQuotedArgument(state) catch break;
+            errdefer state.allocator.free(arg);
+            try params.append(arg);
         }
-    } else |_| {
-        while (true) {
-            const l = self.parseAnyLetter() catch break;
-            try cmd.appendSlice(l);
-        }
+        try parser.endOfInput(state);
+        return .{
+            .allocator = state.allocator,
+            .method_name = method_name,
+            .params = try params.toOwnedSlice(),
+        };
     }
-    return cmd.toOwnedSlice();
-}
+
+    fn argument(state: *parser.State) ![]const u8 {
+        return name(state);
+    }
+
+    fn name(state: *parser.State) ![]const u8 {
+        const in = state.input;
+        errdefer state.input = in;
+        var cs = std.ArrayList(u8).init(state.allocator);
+        errdefer cs.deinit();
+        const head = try nameCharacter(state);
+        try cs.append(head);
+        while (nameCharacter(state)) |c|
+            try cs.append(c)
+        else |_|
+            return cs.toOwnedSlice();
+    }
+
+    fn nameCharacter(state: *parser.State) !u8 {
+        return parser.letter(state) catch parser.digit(state) catch parser.anyCharacter(state, "-_./[]{}");
+    }
+
+    fn doubleQuotedArgument(state: *parser.State) ![]const u8 {
+        const in = state.input;
+        errdefer state.input = in;
+        _ = try parser.anyCharacter(state, "\"");
+        const arg = try argument(state);
+        errdefer state.allocator.free(arg);
+        _ = try parser.anyCharacter(state, "\"");
+        return arg;
+    }
+};
