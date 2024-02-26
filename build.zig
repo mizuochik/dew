823232b6a127cde22ddfe7b1b99de2ff7881041e
Mizuochi Keita <keitam913@yahoo.co.jp>
2024-02-27 07:05:32 +0900

    build.zig: Extract adding commands

--- build.zig
+++ build.zig
@@ -7,35 +7,29 @@ const Options = struct {
 };
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
     const options = .{
-        .target = target,
-        .optimize = optimize,
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
     };
 
-    const ziglyph = b.addStaticLibrary(.{
-        .name = "ziglyph",
-        .root_source_file = .{ .path = "lib/ziglyph/src/ziglyph.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
+    addMain(b, options);
 
+    addTest(b, "all", options);
+    addTest(b, "parser", options);
+    if (std.fs.cwd().access("src/tmp.zig", .{})) |_|
+        addTest(b, "tmp", options)
+    else |_| {}
+}
+
+fn addMain(b: *std.Build, options: Options) void {
     const exe = b.addExecutable(.{
         .name = "dew",
         .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
+        .target = options.target,
+        .optimize = options.optimize,
     });
-    exe.root_module.addImport("ziglyph", &ziglyph.root_module);
-    exe.root_module.addImport("clap", b.dependency("clap", .{
-        .target = target,
-        .optimize = optimize,
-    }).module("clap"));
-
+    addImports(b, &exe.root_module, options);
     b.installArtifact(exe);
-
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
@@ -43,34 +37,29 @@ pub fn build(b: *std.Build) void {
     }
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
-    const tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/tests.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    const run_tests = b.addRunArtifact(tests);
-    const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_tests.step);
-
-    addTest(b, "parser", options);
-    if (std.fs.cwd().access("src/tmp.zig", .{})) |_|
-        addTest(b, "tmp", options)
-    else |_| {}
 }
 
 fn addTest(b: *std.Build, comptime name: []const u8, options: Options) void {
-    const clap = b.dependency("clap", .{
-        .target = options.target,
-        .optimize = options.optimize,
-    });
     const tests = b.addTest(.{
         .root_source_file = .{ .path = "src/" ++ name ++ ".zig" },
         .target = options.target,
         .optimize = options.optimize,
     });
-    tests.root_module.addImport("clap", clap.module("clap"));
+    addImports(b, &tests.root_module, options);
     const run_tests = b.addRunArtifact(tests);
     const test_step = b.step("test-" ++ name, "");
     test_step.dependOn(&run_tests.step);
+}
+
+fn addImports(b: *std.Build, module: *std.Build.Module, options: Options) void {
+    module.addImport("ziglyph", &b.addStaticLibrary(.{
+        .name = "ziglyph",
+        .root_source_file = .{ .path = "lib/ziglyph/src/ziglyph.zig" },
+        .target = options.target,
+        .optimize = options.optimize,
+    }).root_module);
+    module.addImport("clap", b.dependency("clap", .{
+        .target = options.target,
+        .optimize = options.optimize,
+    }).module("clap"));
 }
