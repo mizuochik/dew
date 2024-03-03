14ad062fc5e6bbe0c923a5054f2dd85c028a250f
Mizuochi Keita <keitam913@yahoo.co.jp>
2024-03-04 08:06:04 +0900

    wip

--- src/CommandParser2.zig
+++ src/CommandParser2.zig
@@ -3,21 +3,18 @@ const parser = @import("parser.zig");
 const ModuleDefinition = @import("ModuleDefinition.zig");
 const Command = @import("Command.zig");
 
-pub fn parse(allocator: std.mem.Allocator, definition: ModuleDefinition, input: []const u8) !Command {
+pub fn parse(allocator: std.mem.Allocator, name: []const u8, definition: ModuleDefinition.Command, input: []const u8) !Command {
     var state: parser.State = .{
         .allocator = allocator,
         .input = input,
     };
     parser.spaces(&state) catch {};
-    return command(&state, definition);
+    return command(&state, name, definition);
 }
 
-fn command(state: *parser.State, definition: ModuleDefinition) !Command {
-    std.debug.print("# definition = {s}\n", .{definition.name});
+fn command(state: *parser.State, name: []const u8, definition: ModuleDefinition.Command) !Command {
+    std.debug.print("# definition = {s}\n", .{name});
     std.debug.print("# state.input = {s}\n", .{state.input});
-
-    const name = try parser.string(state, definition.name);
-    errdefer state.allocator.free(name);
 
     const options = try command_options(state, definition);
     errdefer {
@@ -42,7 +39,7 @@ fn command(state: *parser.State, definition: ModuleDefinition) !Command {
     };
 }
 
-fn command_options(state: *parser.State, definition: ModuleDefinition) !std.StringArrayHashMap(Command.Value) {
+fn command_options(state: *parser.State, definition: ModuleDefinition.Command) !std.StringArrayHashMap(Command.Value) {
     const in = state.input;
     errdefer state.input = in;
 
@@ -60,7 +57,7 @@ fn command_options(state: *parser.State, definition: ModuleDefinition) !std.Stri
     }
 
     outer: while (true) {
-        var option_definitions = definition.command.options.iterator();
+        var option_definitions = definition.options.iterator();
         while (option_definitions.next()) |option_definition| {
             _ = parser.string(state, "--") catch continue;
             const key = try parser.string(state, option_definition.key_ptr.*);
@@ -118,7 +115,7 @@ test "parse command" {
     var definition = try ModuleDefinition.parse(std.testing.allocator, @embedFile("builtin_modules/cursors.yaml"));
     errdefer definition.deinit();
 
-    var actual = try @This().parse(std.testing.allocator, definition, "cursors --cursor 1 move --select 10:5");
+    var actual = try @This().parse(std.testing.allocator, definition.name, definition.command, "cursors --cursor 1 move --select 10:5");
     errdefer actual.deinit();
 
     try std.testing.expectEqualStrings("cursors", actual.name);
