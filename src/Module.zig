9d93caaf102b83a2bc37a06bb00688f2ade11c0a
Mizuochi Keita <keitam913@yahoo.co.jp>
2024-03-06 07:30:51 +0900

    Module: Take a command struct instead of a raw string

--- src/Module.zig
+++ src/Module.zig
@@ -2,23 +2,24 @@ const std = @import("std");
 const Keyboard = @import("Keyboard.zig");
 const CommandLine = @import("CommandLine.zig");
 const ModuleDefinition = @import("ModuleDefinition.zig");
+const Command = @import("Command.zig");
 
 pub const Error = error{
     InvalidCommand,
+};
+
+pub const VTable = struct {
+    runCommand: *const fn (ptr: *anyopaque, command: *const Command, input: std.io.AnyReader, output: std.io.AnyWriter) anyerror!void,
+    deinit: *const fn (ptr: *anyopaque) void,
 };
 
 ptr: *anyopaque,
 definition: ModuleDefinition,
 vtable: *const VTable,
 
-pub fn runCommand(self: *@This(), arguments: [][]const u8, input: std.io.AnyReader, output: std.io.AnyWriter) anyerror!void {
-    try self.vtable.apiCommand(self.ptr, arguments, input, output);
+pub fn runCommand(self: *@This(), command: *const Command, input: std.io.AnyReader, output: std.io.AnyWriter) anyerror!void {
+    try self.vtable.runCommand(self.ptr, command, input, output);
 }
-
-pub const VTable = struct {
-    apiCommand: *const fn (ptr: *anyopaque, arguments: [][]const u8, input: std.io.AnyReader, output: std.io.AnyWriter) anyerror!void,
-    deinit: *const fn (ptr: *anyopaque) void,
-};
 
 pub fn deinit(self: *@This()) void {
     self.vtable.deinit(self.ptr);
