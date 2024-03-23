aa5b703a34f58e99cfde814c4ed208d0af5b1683
Mizuochi Keita <keitam913@yahoo.co.jp>
2024-03-23 23:25:30 +0900

    ModuleDefinition: Support subcommands

--- src/ModuleDefinition.zig
+++ src/ModuleDefinition.zig
@@ -1,59 +1,19 @@
 const std = @import("std");
-const yaml = @import("yaml");
 const c = @import("c.zig");
-const ModuleDefinition = @This();
 
 arena: std.heap.ArenaAllocator,
 manifest_version: []const u8,
 name: []const u8,
 description: []const u8,
 command: Command,
-options: []ModuleOption,
-yaml: yaml.Yaml,
+options: []const ModuleOption,
 
 pub const Command = struct {
     name: []const u8,
     description: []const u8,
-    options: []OptionArgument,
-    positionals: []PositionalArgument,
-    subcommands: []Command,
-
-    pub fn fromValue(allocator: std.mem.Allocator, value: yaml.Value, module_name: ?[]const u8) !@This() {
-        const options = b: {
-            var options_al = std.ArrayList(OptionArgument).init(allocator);
-            errdefer options_al.deinit();
-            if (value.map.get("options")) |src_options| {
-                for (src_options.list) |src|
-                    try options_al.append(try OptionArgument.fromValue(src));
-            }
-            break :b try options_al.toOwnedSlice();
-        };
-        errdefer allocator.free(options);
-        const positionals = b: {
-            var positionals = std.ArrayList(PositionalArgument).init(allocator);
-            if (value.map.get("positionals")) |src_positionals|
-                for (src_positionals.list) |src|
-                    try positionals.append(try PositionalArgument.fromValue(src));
-            break :b try positionals.toOwnedSlice();
-        };
-        errdefer allocator.free(positionals);
-        const subcommands = b: {
-            var subcommands = std.ArrayList(Command).init(allocator);
-            errdefer subcommands.deinit();
-            if (value.map.get("subcommands")) |src_subcommands|
-                for (src_subcommands.list) |src|
-                    try subcommands.append(try Command.fromValue(allocator, src, null));
-            break :b try subcommands.toOwnedSlice();
-        };
-        errdefer allocator.free(subcommands);
-        return .{
-            .name = module_name orelse value.map.get("name").?.string,
-            .description = value.map.get("description").?.string,
-            .options = options,
-            .positionals = positionals,
-            .subcommands = subcommands,
-        };
-    }
+    options: []const OptionArgument,
+    positionals: []const PositionalArgument,
+    subcommands: []const Command,
 };
 
 pub const ValueType = enum {
@@ -61,19 +21,6 @@ pub const ValueType = enum {
     float,
     str,
     bool_,
-
-    pub fn from(source: []const u8) !@This() {
-        return if (std.mem.eql(u8, source, "int"))
-            .int
-        else if (std.mem.eql(u8, source, "float"))
-            .float
-        else if (std.mem.eql(u8, source, "str"))
-            .str
-        else if (std.mem.eql(u8, source, "bool"))
-            .bool_
-        else
-            return error.InvalidYaml;
-    }
 };
 
 pub const OptionArgument = struct {
@@ -81,27 +28,7 @@ pub const OptionArgument = struct {
     short: ?[]const u8,
     type: ValueType,
     description: []const u8,
-    default: DefaultValue,
-
-    pub fn fromValue(value: yaml.Value) !@This() {
-        const value_type = try ValueType.from(value.map.get("type").?.string);
-        return .{
-            .long = if (value.map.get("long")) |v|
-                v.string
-            else
-                null,
-            .short = if (value.map.get("short")) |v|
-                v.string
-            else
-                null,
-            .type = value_type,
-            .default = switch (value_type) {
-                .bool_ => .{ .bool_ = false },
-                else => try DefaultValue.from(value.map.get("default").?),
-            },
-            .description = value.map.get("description").?.string,
-        };
-    }
+    default: ?DefaultValue,
 };
 
 pub const DefaultValue = union(enum) {
@@ -109,29 +36,12 @@ pub const DefaultValue = union(enum) {
     float: f64,
     str: []const u8,
     bool_: bool,
-
-    pub fn from(source: yaml.Value) !@This() {
-        return switch (source) {
-            .int => |i| .{ .int = i },
-            .float => |f| .{ .float = f },
-            .string => |s| .{ .str = s },
-            else => return error.InvalidYaml,
-        };
-    }
 };
 
 pub const PositionalArgument = struct {
     name: []const u8,
     type: ValueType,
     description: []const u8,
-
-    pub fn fromValue(value: yaml.Value) !@This() {
-        return .{
-            .name = value.map.get("name").?.string,
-            .description = value.map.get("description").?.string,
-            .type = try ValueType.from(value.map.get("type").?.string),
-        };
-    }
 };
 
 pub const ModuleOption = struct {
@@ -139,53 +49,18 @@ pub const ModuleOption = struct {
     type_: ValueType,
     description: []const u8,
     default: DefaultValue,
-
-    pub fn fromValue(value: yaml.Value) !@This() {
-        return .{
-            .name = value.map.get("name").?.string,
-            .description = value.map.get("description").?.string,
-            .type_ = try ValueType.from(value.map.get("type").?.string),
-            .default = undefined,
-        };
-    }
 };
-
-pub fn fromValue(y: *yaml.Yaml, value: yaml.Value) !@This() {
-    var definition: @This() = .{
-        .arena = undefined,
-        .yaml = undefined,
-        .manifest_version = value.map.get("manifest_version").?.string,
-        .name = value.map.get("name").?.string,
-        .description = value.map.get("description").?.string,
-        .options = b: {
-            var options = std.ArrayList(ModuleOption).init(y.arena.allocator());
-            errdefer options.deinit();
-            if (value.map.get("options")) |src_options|
-                for (src_options.list) |src_option|
-                    try options.append(try ModuleOption.fromValue(src_option));
-            break :b try options.toOwnedSlice();
-        },
-        .command = try Command.fromValue(y.arena.allocator(), value.map.get("command").?, value.map.get("name").?.string),
-    };
-    definition.yaml = y.*;
-    return definition;
-}
 
 pub fn deinit(self: *@This()) void {
     self.arena.deinit();
 }
 
 pub fn parse(allocator: std.mem.Allocator, source: []const u8) !@This() {
-    var y = try yaml.Yaml.load(allocator, source);
-    errdefer y.deinit();
-    return try @This().fromValue(&y, y.docs.items[0]);
-}
-
-pub fn parseByLibYaml(allocator: std.mem.Allocator, source: []const u8) !ModuleDefinition {
     var parser = try Parser.init(allocator, source);
-    defer parser.deinit();
     return parser.moduleDefinition();
 }
+
+const ModuleDefinition = @This();
 
 const Parser = struct {
     const Error = error{
@@ -196,7 +71,7 @@ const Parser = struct {
     arena: std.heap.ArenaAllocator,
     yaml_parser: c.yaml_parser_t,
 
-    fn init(allocator: std.mem.Allocator, input: []const u8) !Parser {
+    fn init(allocator: std.mem.Allocator, input: []const u8) !@This() {
         var parser: c.yaml_parser_t = undefined;
         _ = c.yaml_parser_initialize(&parser);
         errdefer c.yaml_parser_delete(&parser);
@@ -207,11 +82,11 @@ const Parser = struct {
         };
     }
 
-    fn deinit(self: *Parser) void {
+    fn deinit(self: *@This()) void {
         c.yaml_parser_delete(&self.yaml_parser);
     }
 
-    fn moduleDefinition(self: *Parser) !ModuleDefinition {
+    fn moduleDefinition(self: *@This()) !ModuleDefinition {
         errdefer self.arena.deinit();
 
         var manifest_version: ?[]const u8 = null;
@@ -279,11 +154,10 @@ const Parser = struct {
             .description = description orelse return Error.UnexpectedInput,
             .command = command_ orelse return Error.UnexpectedInput,
             .options = module_options orelse return Error.UnexpectedInput,
-            .yaml = undefined,
         };
     }
 
-    fn moduleOptions(self: *Parser) ![]ModuleOption {
+    fn moduleOptions(self: *@This()) ![]ModuleOption {
         {
             var event: c.yaml_event_s = undefined;
             if (c.yaml_parser_parse(&self.yaml_parser, &event) != 1)
@@ -308,7 +182,7 @@ const Parser = struct {
         return options.toOwnedSlice();
     }
 
-    fn moduleOption(self: *Parser) !ModuleOption {
+    fn moduleOption(self: *@This()) !ModuleOption {
         var name: ?[]const u8 = null;
         var type_: ?ValueType = null;
         var description: ?[]const u8 = null;
@@ -342,7 +216,7 @@ const Parser = struct {
         };
     }
 
-    fn scalarString(self: *Parser) ![]const u8 {
+    fn scalarString(self: *@This()) ![]const u8 {
         var event: c.yaml_event_s = undefined;
         if (c.yaml_parser_parse(&self.yaml_parser, &event) != 1)
             return Error.YamlParseError;
@@ -352,7 +226,7 @@ const Parser = struct {
         return self.arena.allocator().dupe(u8, event.data.scalar.value[0..event.data.scalar.length]);
     }
 
-    fn valueType(self: *Parser) !ValueType {
+    fn valueType(self: *@This()) !ValueType {
         const s = try self.scalarString();
         return if (std.mem.eql(u8, s, "int"))
             .int
@@ -366,7 +240,7 @@ const Parser = struct {
             Error.UnexpectedInput;
     }
 
-    fn defaultValue(self: *Parser) !DefaultValue {
+    fn defaultValue(self: *@This()) !DefaultValue {
         var event: c.yaml_event_s = undefined;
         if (c.yaml_parser_parse(&self.yaml_parser, &event) != 1)
             return Error.YamlParseError;
@@ -382,8 +256,8 @@ const Parser = struct {
             .{ .str = try self.arena.allocator().dupe(u8, source) };
     }
 
-    fn command(self: *Parser, module_name: ?[]const u8) !Command {
-        {
+    fn command(self: *@This(), module_name: ?[]const u8) !Command {
+        if (module_name) |_| {
             var event: c.yaml_event_s = undefined;
             if (c.yaml_parser_parse(&self.yaml_parser, &event) != 1)
                 return Error.YamlParseError;
@@ -393,7 +267,9 @@ const Parser = struct {
         }
         var description: ?[]const u8 = null;
         var options: ?[]OptionArgument = null;
+        var positionals: ?[]PositionalArgument = null;
         var name: ?[]const u8 = module_name;
+        var subcommands_: ?[]Command = null;
         while (true) {
             var event: c.yaml_event_s = undefined;
             if (c.yaml_parser_parse(&self.yaml_parser, &event) != 1)
@@ -412,24 +288,25 @@ const Parser = struct {
                 else if (std.mem.eql(u8, key, "options"))
                     options = try self.optionArguments()
                 else if (std.mem.eql(u8, key, "subcommands"))
-                    break
-                else {
+                    subcommands_ = try self.subcommands()
+                else if (std.mem.eql(u8, key, "positionals"))
+                    positionals = try self.positionalArguments()
+                else
                     unreachable;
-                }
                 continue;
             }
             return Error.UnexpectedInput;
         }
-        return Command{
+        return .{
             .name = name orelse return Error.UnexpectedInput,
             .description = description orelse return Error.UnexpectedInput,
-            .options = options orelse return Error.UnexpectedInput,
-            .positionals = undefined,
-            .subcommands = undefined,
+            .options = options orelse try self.arena.allocator().alloc(OptionArgument, 0),
+            .positionals = positionals orelse try self.arena.allocator().alloc(PositionalArgument, 0),
+            .subcommands = subcommands_ orelse try self.arena.allocator().alloc(Command, 0),
         };
     }
 
-    fn optionArguments(self: *Parser) ![]OptionArgument {
+    fn optionArguments(self: *@This()) ![]OptionArgument {
         {
             var event: c.yaml_event_s = undefined;
             if (c.yaml_parser_parse(&self.yaml_parser, &event) != 1)
@@ -452,7 +329,7 @@ const Parser = struct {
         return try arguments.toOwnedSlice();
     }
 
-    fn optionArgument(self: *Parser) !OptionArgument {
+    fn optionArgument(self: *@This()) !OptionArgument {
         var long: ?[]const u8 = null;
         var short: ?[]const u8 = null;
         var type_: ?ValueType = null;
@@ -484,31 +361,152 @@ const Parser = struct {
             return Error.UnexpectedInput;
         }
         return .{
-            .long = long orelse return Error.UnexpectedInput,
-            .short = short orelse return Error.UnexpectedInput,
+            .long = long,
+            .short = short,
             .type = type_ orelse return Error.UnexpectedInput,
             .description = description orelse return Error.UnexpectedInput,
-            .default = default orelse return Error.UnexpectedInput,
+            .default = default,
         };
+    }
+
+    fn positionalArguments(self: *@This()) ![]PositionalArgument {
+        {
+            var event: c.yaml_event_s = undefined;
+            if (c.yaml_parser_parse(&self.yaml_parser, &event) != 1)
+                return Error.YamlParseError;
+            defer c.yaml_event_delete(&event);
+            if (event.type != c.YAML_SEQUENCE_START_EVENT)
+                return Error.UnexpectedInput;
+        }
+        var arguments = std.ArrayList(PositionalArgument).init(self.arena.allocator());
+        while (true) {
+            var event: c.yaml_event_s = undefined;
+            if (c.yaml_parser_parse(&self.yaml_parser, &event) != 1)
+                return Error.YamlParseError;
+            defer c.yaml_event_delete(&event);
+            if (event.type == c.YAML_SEQUENCE_END_EVENT)
+                break;
+            if (event.type == c.YAML_MAPPING_START_EVENT)
+                try arguments.append(try self.positionalArgument());
+        }
+        return try arguments.toOwnedSlice();
+    }
+
+    fn positionalArgument(self: *@This()) !PositionalArgument {
+        var name: ?[]const u8 = null;
+        var type_: ?ValueType = null;
+        var description: ?[]const u8 = null;
+        while (true) {
+            var event: c.yaml_event_s = undefined;
+            if (c.yaml_parser_parse(&self.yaml_parser, &event) != 1)
+                return Error.YamlParseError;
+            defer c.yaml_event_delete(&event);
+            if (event.type == c.YAML_MAPPING_END_EVENT)
+                break;
+            if (event.type == c.YAML_SCALAR_EVENT) {
+                const key = event.data.scalar.value[0..event.data.scalar.length];
+                if (std.mem.eql(u8, key, "name"))
+                    name = try self.scalarString()
+                else if (std.mem.eql(u8, key, "type"))
+                    type_ = try self.valueType()
+                else if (std.mem.eql(u8, key, "description"))
+                    description = try self.scalarString()
+                else
+                    return Error.UnexpectedInput;
+                continue;
+            }
+            return Error.UnexpectedInput;
+        }
+        return .{
+            .name = name orelse return Error.UnexpectedInput,
+            .type = type_ orelse return Error.UnexpectedInput,
+            .description = description orelse return Error.UnexpectedInput,
+        };
+    }
+
+    fn subcommands(self: *@This()) anyerror![]Command {
+        {
+            var event: c.yaml_event_s = undefined;
+            if (c.yaml_parser_parse(&self.yaml_parser, &event) != 1)
+                return Error.YamlParseError;
+            defer c.yaml_event_delete(&event);
+            if (event.type != c.YAML_SEQUENCE_START_EVENT)
+                return Error.UnexpectedInput;
+        }
+        var commands = std.ArrayList(Command).init(self.arena.allocator());
+        while (true) {
+            var event: c.yaml_event_s = undefined;
+            if (c.yaml_parser_parse(&self.yaml_parser, &event) != 1)
+                return Error.YamlParseError;
+            defer c.yaml_event_delete(&event);
+            if (event.type == c.YAML_SEQUENCE_END_EVENT)
+                break;
+            if (event.type == c.YAML_MAPPING_START_EVENT)
+                try commands.append(try self.command(null));
+        }
+        return try commands.toOwnedSlice();
     }
 };
 
 test "parseByLibYaml" {
-    var definition = try ModuleDefinition.parseByLibYaml(std.testing.allocator, @embedFile("builtin_modules/cursors.yaml"));
-    defer definition.deinit();
-    try std.testing.expectEqualStrings("0.1", definition.manifest_version);
-    try std.testing.expectEqualStrings("selections", definition.name);
-    try std.testing.expectEqualStrings("Selections in an editor", definition.description);
-    try std.testing.expectEqual(1, definition.options.len);
-    try std.testing.expectEqualStrings("hello", definition.options[0].name);
-    try std.testing.expectEqual(ValueType.str, definition.options[0].type_);
-    try std.testing.expectEqualStrings("foo", definition.options[0].description);
-    try std.testing.expectEqualDeep(DefaultValue{ .str = "*" }, definition.options[0].default);
-    try std.testing.expectEqualStrings("Control selections", definition.command.description);
-    try std.testing.expectEqual(1, definition.command.options.len);
-    try std.testing.expectEqualStrings("selection", definition.command.options[0].long.?);
-    try std.testing.expectEqualStrings("c", definition.command.options[0].short.?);
-    try std.testing.expectEqual(ValueType.str, definition.command.options[0].type);
-    try std.testing.expectEqualStrings("Selection id", definition.command.options[0].description);
-    try std.testing.expectEqualDeep(DefaultValue{ .str = "*" }, definition.command.options[0].default);
+    var actual = try @This().parse(std.testing.allocator, @embedFile("builtin_modules/cursors.yaml"));
+    defer actual.deinit();
+    try std.testing.expectEqualDeep(ModuleDefinition{
+        .arena = actual.arena,
+        .manifest_version = "0.1",
+        .name = "selections",
+        .description = "Selections in an editor",
+        .options = &[_]ModuleOption{
+            .{
+                .name = "hello",
+                .type_ = .str,
+                .description = "foo",
+                .default = .{ .str = "*" },
+            },
+        },
+        .command = .{
+            .name = "selections",
+            .description = "Control selections",
+            .options = &[_]OptionArgument{
+                .{
+                    .long = "selection",
+                    .short = "c",
+                    .type = ValueType.str,
+                    .description = "Selection id",
+                    .default = .{ .str = "*" },
+                },
+            },
+            .positionals = &[_]PositionalArgument{},
+            .subcommands = &[_]Command{
+                .{
+                    .name = "get",
+                    .description = "Get selection info",
+                    .options = &[_]OptionArgument{},
+                    .positionals = &[_]PositionalArgument{},
+                    .subcommands = &[_]Command{},
+                },
+                .{
+                    .name = "move",
+                    .description = "Move specified selections",
+                    .options = &[_]OptionArgument{
+                        .{
+                            .long = "select",
+                            .short = null,
+                            .type = ValueType.bool_,
+                            .description = "Enable selecting",
+                            .default = null,
+                        },
+                    },
+                    .positionals = &[_]PositionalArgument{
+                        .{
+                            .name = "position",
+                            .type = .str,
+                            .description = "Target position",
+                        },
+                    },
+                    .subcommands = &[_]Command{},
+                },
+            },
+        },
+    }, actual);
 }
